17d8428801027dbc9b17bdf3ef37348e61dd01db
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 14:42:25 +0100

    Main with a simple menu, created a Question class with 3 test questions in an array,

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        //scanner
+        Scanner scan = new Scanner(System.in);
+
+        //asks for user input to ether start a game or quit
+        while(true) {
+
+            System.out.println("Type:\n1: Multiple Choice Question \n2: Quit");
+
+            int menuInput = scan.nextInt(); //takes in user input of int
+
+            if(menuInput == 1){
+
+                System.out.println("Here is question");
+
+            }else if(menuInput == 2){
+
+                System.out.println("BYE!");
+
+                break;
+
+            } else {
+
+                System.out.println("You need to input 1 or 2 to answer in this menu! Try again! ");
+            }
+
+        }
+
     }
 }
---
+++ src/Question.java
@@ -0,0 +1,38 @@
+/**
+ * Created by NAME HERE
+ * Date: 12/11/2020
+ * Time: 13:20
+ * Project: QuizkampenKopia
+ * Copyright: MIT
+ **/
+public class Question {
+
+    //a string array that represent a couple of  row in a TXT file that's on the server or something
+    //Question, answer, option1, option2, option3, option4;
+    String[][] Question = {{"Who's branch is this?", "Lukas", "Lukas", "Jesper", "Pavel", "David"},//hodei was offline on discord
+            {"What Los Angeles community is noted for celebrities and mansions?","Beverly Hills","Nob Hill","Beverly Hills","Chestnut Hill","Bunker Hill"}, //stole form www.q4quiz.com
+            {"Substances that have fast-moving particles that are far apart, and have no definite volume nor shape are:","Gases","Gases","Liquids","Solids","Other"}, //stole form www.q4quiz.com ||onLy had 3 options so i added other
+    };
+
+
+
+
+
+    //"Who's branch is this?, Lukas, Lukas, Jesper, Pavel, David";//hodei was offline on discord
+
+    int[] answerTrack = new int[4];
+
+    String[] Options = new String[4];
+
+
+
+
+
+
+
+
+
+
+
+
+}
