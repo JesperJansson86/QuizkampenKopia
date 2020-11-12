369603f8eaf00e4b318c15aa638abf0f51f7eb8e
hodeiez <hodeinaiz@hotmail.com>
2020-11-12 20:20:54 +0100

    show red if answer is wrong and show right answer with green. added shuffle (reset) question button

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,30 +13,60 @@ public class Controller {
     public Button answer3;
     public Button answer4;
     public Label question;
+    public AnchorPane mainPane;
     Question q;
+    ArrayList<String> answers = new ArrayList<>();
+    ArrayList<Button> buttons = new ArrayList<>();
+    Button right;
 
-
-    public void initialize(){
-        q=new Question("Which one is right?","right","left2","left3","left4");
+    public void initialize() {
+        q = new Question("Which one is right?", "right", "left2", "left3", "left4");
         question.setText(q.getQuestion());
-        ArrayList<String> answers=new ArrayList<>();
+
         answers.add(q.getAnswer2());
         answers.add(q.getAnswer3());
         answers.add(q.getAnswer4());
         answers.add(q.getRightAnswer());
+
+       setShuffled();
+
+    }
+
+
+    public void answerOn(ActionEvent actionEvent) {
+        System.out.println(right.getId());
+        Button buttonCLicked = ((Button) actionEvent.getSource());
+        if (buttonCLicked.getText().equals(q.getRightAnswer())) {
+            buttonCLicked.setStyle("-fx-background-color: green");
+        } else if (!buttonCLicked.getText().equals(q.getRightAnswer())) {
+            buttonCLicked.setStyle("-fx-background-color: red");
+            right.setStyle("-fx-background-color: green");
+        }
+    }
+
+    public void resetTest(ActionEvent actionEvent) {
+       for(Button button:buttons){
+           button.setStyle(null);
+       }
+        setShuffled();
+    }
+    public void setShuffled(){
         Collections.shuffle(answers);
+
         answer1.setText(answers.get(0));
         answer2.setText(answers.get(1));
         answer3.setText(answers.get(2));
         answer4.setText(answers.get(3));
-    }
-    public void answerOn(ActionEvent actionEvent) {
-        System.out.println( actionEvent.getSource());
-        System.out.println();
-        Button buttonCLicked=((Button)actionEvent.getSource());
-if(buttonCLicked.getText().equals(q.getRightAnswer())){
-    buttonCLicked.setStyle("-fx-background-color: green");
-}
+
+        buttons.add(answer1);
+        buttons.add(answer2);
+        buttons.add(answer3);
+        buttons.add(answer4);
+
+        for (Button b : buttons) {
+            if (b.getText().equals(q.getRightAnswer()))
+                right = b;
+        }
     }
 }
 
