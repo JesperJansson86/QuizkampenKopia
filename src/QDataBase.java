0a49b81d6a1e411af601b33eabf5414cf698eedc
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2020-11-12 19:09:58 +0100

    Gjort en väldigt simpel version av ett singleplayerspel. Klassen Question samt en databas med 3 Questions.

---
+++ src/QDataBase.java
@@ -0,0 +1,10 @@
+public class QDataBase {
+    public Question q1 = new Question("Två plus två?","Fyra","Tre","Två","fem");
+    public Question q2 = new Question("Två minus två?","Noll","Tre","Två","fem");
+    public Question q3 = new Question("Två är lika med?","Två","Tre","Fyra","fem");
+
+    public Question getQuestion(Question q){
+        return q;
+    }
+
+}
---
+++ src/Question.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Question {
+
+    String question;
+    String correctAnswer;
+    String incorrect1;
+    String incorrect2;
+    String incorrect3;
+    List<String> shuffled = new LinkedList<>();
+    public Question(String question,String correctAnswer,String incorrect1,String incorrect2,String incorrect3)
+    {
+     this.question=question;
+     this.correctAnswer=correctAnswer;
+     this.incorrect1=incorrect1;
+     this.incorrect1=incorrect2;
+     this.incorrect1=incorrect3;
+     shuffled.add(correctAnswer);
+     shuffled.add(incorrect1);
+     shuffled.add(incorrect2);
+     shuffled.add(incorrect3);
+     Collections.shuffle(shuffled);
+    }
+    public Question(){
+
+    }
+
+    @Override
+    public String toString() {
+        String printOutput ="";
+        for (int i = 0; i < 4; i++) {
+           printOutput = printOutput+" / "+ shuffled.get(i);
+        }
+        return printOutput;
+    }
+}
