b09f9e3b3b3c449c45c0b0dfb91c738f0db645ff
BuildTools <unconfigured@null.spigotmc.org>
2020-11-12 19:58:05 +0100

    Randomizes and asks a question, gives 4 options that are in random order, user can type answer (that is not yet checked)

--- src/Main.java
+++ src/Main.java
@@ -18,16 +18,21 @@ public class Main {
 
             if(menuInput == 1){
 
+                //here it will call the question class
                 System.out.println("Here is question");
+
+                new Question();
 
             }else if(menuInput == 2){
 
+                //quits the while loop and there for the thread will end.
                 System.out.println("BYE!");
 
                 break;
 
             } else {
 
+                // if the user somehow messes up typing 1 or 2 into the console
                 System.out.println("You need to input 1 or 2 to answer in this menu! Try again! ");
             }
 
--- src/Question.java
+++ src/Question.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Created by NAME HERE
  * Date: 12/11/2020
@@ -9,20 +11,77 @@ public class Question {
 
     //a string array that represent a couple of  row in a TXT file that's on the server or something
     //Question, answer, option1, option2, option3, option4;
-    String[][] Question = {{"Who's branch is this?", "Lukas", "Lukas", "Jesper", "Pavel", "David"},//hodei was offline on discord
-            {"What Los Angeles community is noted for celebrities and mansions?","Beverly Hills","Nob Hill","Beverly Hills","Chestnut Hill","Bunker Hill"}, //stole form www.q4quiz.com
+    String[][] question = {
+            {"Who's branch is this?", "Lukas", "Lukas", "Jesper", "Pavel", "David"},//hodei was offline on discord
+            {"What Los Angeles community is noted for celebrities and mansions?", "Beverly Hills","Nob Hill","Beverly Hills","Chestnut Hill","Bunker Hill"}, //stole form www.q4quiz.com
             {"Substances that have fast-moving particles that are far apart, and have no definite volume nor shape are:","Gases","Gases","Liquids","Solids","Other"}, //stole form www.q4quiz.com ||onLy had 3 options so i added other
     };
 
 
+    //creates an instance of the class Random
+    Random random = new Random();
+
+
+    public Question(){
+
+        Scanner scan = new Scanner(System.in);
+
+        int questionRand = questionRand(); //holds the question
+        Integer[] optionRand = optionRand(); //holds the order of the options
+
+        //prints out the question then the options in the order of the optionRand
+        System.out.println(question[questionRand][0] //prints out the question
+                        + "\n1: " + question[questionRand][optionRand[0]] //prints out option 1
+                        + "\n2: " + question[questionRand][optionRand[1]] //prints out option 2
+                        + "\n3: " + question[questionRand][optionRand[2]] //prints out option 3
+                        + "\n4: " + question[questionRand][optionRand[3]] //prints out option 4
+        );
+
+        int answer = scan.nextInt(); //takes in user input that is hopefully an int
+
+        scanCheck(answer); //Checks user input for correct or wrong answer
+    }
+
+
+    /**
+     * Decides witch question in the array that will be chosen.
+     * @return returns a number that will later be used to decide witch question will be asked.
+     */
+    public int questionRand(){
+
+        //random between the Question.length() to decide witch question
+        return random.nextInt(question.length);
+    }
+
+    /**
+     * Decides what order the options will be in.
+     * @return returns an array of Integers in an order witch will decide what order the options will be.
+     */
+    public Integer[] optionRand(){
+
+        Integer[] optionOrder = new Integer[]{2,3,4,5}; // an array with the indexes of the answer options for the question
+
+        List<Integer> shuffle = Arrays.asList(optionOrder); //makes a list called shuffle
+
+        Collections.shuffle(shuffle); //shuffles the list
+
+        Integer[] shuffled = new Integer[shuffle.size()]; //the array that will be sent back but with out any values
+
+        shuffled = shuffle.toArray(shuffled); //gives shuffled the values of shuffle
+
+        return shuffled;
+
+    }
 
 
 
-    //"Who's branch is this?, Lukas, Lukas, Jesper, Pavel, David";//hodei was offline on discord
+    public void scanCheck(int answer){
 
-    int[] answerTrack = new int[4];
+        System.out.println(answer + " was the user inputted answer ");
 
-    String[] Options = new String[4];
+    }
+
+
 
 
 
