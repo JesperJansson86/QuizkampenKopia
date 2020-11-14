d78f49f2e374ced45f1d8c8668fd40ff40af4663
hodeiez <hodeinaiz@hotmail.com>
2020-11-14 23:58:21 +0100

    QuestionPanel working, have to CLEAN code

--- src/GUI/controllers/QuestionsPanel.java
+++ src/GUI/controllers/QuestionsPanel.java
@@ -4,6 +4,7 @@ import GUI.models.GUIutils;
 import MainClasses.Question;
 import MainClasses.QuestionFactory;
 import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
@@ -39,49 +40,69 @@ public class QuestionsPanel {
     public Label roundNumber;
     public ProgressBar timeLeft;
 
-
-
+    int round = 1;
+    //the methods of this variables can go to model, to keep controller a bit cleaner
     Question q;
     ArrayList<String> answers = new ArrayList<>();
     ArrayList<Button> buttons = new ArrayList<>();
     QuestionFactory questionsAndAnswers = new QuestionFactory();
     List<Question> questionList;
+    List<Rectangle>resultsList=new ArrayList<>();
     Button right;
+    int timelinePosition=0;
+
     @FXML
     Label categoryL;
-    static double progressTime=1;
 
     GUIutils utils;
+
     /**
      * at start initiates questionList by copying the list from MainClasses.QuestionFactory
      */
     public void initialize() {
-        utils=new GUIutils(mainPane);
+        resultsList.add(Qresult2);
+        resultsList.add(Qresult1);
+
+
+
+        roundNumber.setText(String.valueOf(round));
+        utils = new GUIutils(mainPane);
         questionList = new ArrayList<>(questionsAndAnswers.getQuestionList());
-       timeLeft.setProgress(1);
+        timeLeft.setProgress(1);
         setOnStage();
-
+        timeBar(5);
         timeForAnswer();
-
-
     }
-public void timeForAnswer(){
-    Timeline timeline = new Timeline(
-            new KeyFrame(Duration.seconds(5), e -> {
-                resetTest(e);
-                setOnStage();
-            }),
-    new KeyFrame(Duration.seconds(10), e -> {
-        try {
-            utils.changeScene("../view/ResultsAndReview.fxml");
-        } catch (IOException ioException) {
-            ioException.printStackTrace();
-        }
-    }));
 
-    timeline.play();
-}
+    public void timeBar(int seconds) {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(timeLeft.progressProperty(), 1)),
+                new KeyFrame(Duration.seconds(seconds), e -> {
 
+                }, new KeyValue(timeLeft.progressProperty(), 0)));
+        timeline.play();
+    }
+
+    //added a game sequence in time,
+    public void timeForAnswer() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(5), e -> {
+                    reset(e);
+                    setOnStage();
+                    timeBar(5);
+                }),
+                new KeyFrame(Duration.seconds(10), e -> {
+                    try {
+                        utils.changeScene("../view/ResultsAndReview.fxml");
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }));
+
+        timeline.play();
+    }
+
+    //TODO: Need a method which jumps to next question when user answers.
     /**
      * actionlistener of question options. if its right it paints green, if not it paints red and paints green right answer
      *
@@ -92,17 +113,19 @@ public void timeForAnswer(){
         if (buttonCLicked.getText().equals(q.getRightAnswer())) {
             buttonCLicked.setStyle("-fx-background-color: green");
             transition(buttonCLicked, null);
+            resultsList.get(questionList.size()).setStyle("-fx-fill: green");
 
         } else if (!buttonCLicked.getText().equals(q.getRightAnswer())) {
             buttonCLicked.setStyle("-fx-background-color: red");
             transition(right, "-fx-background-color: green");
             transition(buttonCLicked, null);
+            resultsList.get(questionList.size()).setStyle("-fx-fill: red");
 
         }
     }
 
     public void transition(Button button, String style) {
-        PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+        PauseTransition pause = new PauseTransition(Duration.seconds(1));
         pause.setOnFinished(e -> {
             button.setStyle(style);
         });
@@ -114,14 +137,14 @@ public void timeForAnswer(){
      *
      * @param actionEvent
      */
-    public void resetTest(ActionEvent actionEvent) {
+    public void reset(ActionEvent actionEvent) {
         for (Button button : buttons) {
             button.setStyle(null);
         }
         buttons.clear();
         answers.clear();
         question.setText(null); //this is not necessary, but, just in case, and its only one line
-     //   setOnStage();
+        //   setOnStage();
 
     }
 
@@ -166,6 +189,15 @@ public void timeForAnswer(){
         categoryL.setText(q.getCategory());
         setShuffled();
     }
+
+    //remade method, to not take randomly from the list. I suppose it comes random from the server??
+    public Question takeAquestion() {
+
+        Question question = questionList.get(questionList.size() - 1);
+        questionList.remove(questionList.size() - 1);
+
+        return question;
+    }
 /*
     public Question takeAquestion() {
         refillListIfEmpty();
@@ -177,19 +209,17 @@ public void timeForAnswer(){
         return question;
     }
 
- */
-public Question takeAquestion() {
 
-    Question question = questionList.get(questionList.size()-1);
-    questionList.remove(questionList.size()-1);
 
-    return question;
-}
+
+
     public void refillListIfEmpty() {
         if (questionList.size() < 1) {
             questionList = new ArrayList<>(questionsAndAnswers.getQuestionList());
         }
     }
+
+ */
 
 
 }
