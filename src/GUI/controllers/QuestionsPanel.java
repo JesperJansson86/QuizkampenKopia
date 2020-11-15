22094eaf523d7b20abdc5f7807d329b9afeaf850
hodeiez <hodeinaiz@hotmail.com>
2020-11-15 13:22:30 +0100

    separation of concerns in QUestionPanel, now logic is in its model

--- src/GUI/controllers/QuestionsPanel.java
+++ src/GUI/controllers/QuestionsPanel.java
@@ -1,12 +1,10 @@
 package GUI.controllers;
 
 import GUI.models.GUIutils;
+import GUI.models.QuestionsPanelModel;
 import MainClasses.Question;
 import MainClasses.QuestionFactory;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.PauseTransition;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -34,23 +32,20 @@ public class QuestionsPanel {
     public Button answer4;
     public Label question;
     public AnchorPane mainPane;
+    //TODO:add this rectangle programmatically depending on amount of questions
     public Rectangle Qresult1;
     public Rectangle Qresult2;
     //public Rectangle Qresult3;
     public Label roundNumber;
     public ProgressBar timeLeft;
+    public Rectangle rectangleQ;
 
+    //this should come from somewhere else (game class??)
     int round = 1;
-    //the methods of this variables can go to model, to keep controller a bit cleaner
-    Question q;
-    ArrayList<String> answers = new ArrayList<>();
-    ArrayList<Button> buttons = new ArrayList<>();
-    QuestionFactory questionsAndAnswers = new QuestionFactory();
-    List<Question> questionList;
-    List<Rectangle>resultsList=new ArrayList<>();
-    Button right;
-    int timelinePosition=0;
 
+    List<Rectangle>resultsList=new ArrayList<>();
+
+    QuestionsPanelModel model;
     @FXML
     Label categoryL;
 
@@ -60,16 +55,18 @@ public class QuestionsPanel {
      * at start initiates questionList by copying the list from MainClasses.QuestionFactory
      */
     public void initialize() {
+        model=new QuestionsPanelModel(answer1,answer2,answer3,answer4,categoryL,question);
         resultsList.add(Qresult2);
         resultsList.add(Qresult1);
 
-
-
         roundNumber.setText(String.valueOf(round));
         utils = new GUIutils(mainPane);
-        questionList = new ArrayList<>(questionsAndAnswers.getQuestionList());
+      //  questionList = new ArrayList<>(questionsAndAnswers.getQuestionList());
         timeLeft.setProgress(1);
-        setOnStage();
+
+        //this have to be in one method.
+        model.setOnStage();
+        animationTest();
         timeBar(5);
         timeForAnswer();
     }
@@ -83,12 +80,14 @@ public class QuestionsPanel {
         timeline.play();
     }
 
-    //added a game sequence in time,
+    //added a game sequence in time, make a method which creates a timeline depending on amount of questions.
     public void timeForAnswer() {
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(5), e -> {
-                    reset(e);
-                    setOnStage();
+                    model.reset(e);
+                    resetAnimationTest();
+                    model.setOnStage();
+                    animationTest();
                     timeBar(5);
                 }),
                 new KeyFrame(Duration.seconds(10), e -> {
@@ -102,7 +101,7 @@ public class QuestionsPanel {
         timeline.play();
     }
 
-    //TODO: Need a method which jumps to next question when user answers.
+    //TODO: Need a method which jumps to next question when user answers. and make this cleaner
     /**
      * actionlistener of question options. if its right it paints green, if not it paints red and paints green right answer
      *
@@ -110,16 +109,16 @@ public class QuestionsPanel {
      */
     public void answerOn(ActionEvent actionEvent) {
         Button buttonCLicked = ((Button) actionEvent.getSource());
-        if (buttonCLicked.getText().equals(q.getRightAnswer())) {
+        if (buttonCLicked.getText().equals(model.getQ().getRightAnswer())) {
             buttonCLicked.setStyle("-fx-background-color: green");
             transition(buttonCLicked, null);
-            resultsList.get(questionList.size()).setStyle("-fx-fill: green");
+            resultsList.get(model.getQuestionList().size()).setStyle("-fx-fill: green");
 
-        } else if (!buttonCLicked.getText().equals(q.getRightAnswer())) {
+        } else if (!buttonCLicked.getText().equals(model.getQ().getRightAnswer())) {
             buttonCLicked.setStyle("-fx-background-color: red");
-            transition(right, "-fx-background-color: green");
+            transition(model.getRight(), "-fx-background-color: green");
             transition(buttonCLicked, null);
-            resultsList.get(questionList.size()).setStyle("-fx-fill: red");
+            resultsList.get(model.getQuestionList().size()).setStyle("-fx-fill: red");
 
         }
     }
@@ -131,96 +130,24 @@ public class QuestionsPanel {
         });
         pause.play();
     }
+    public void animationTest(){
+        TranslateTransition st=new TranslateTransition(Duration.millis(1000), rectangleQ);
+        st.setByY(-200);
+        st.play();
+    }
+    public void resetAnimationTest(){
+        rectangleQ.setTranslateY(0);
+    }
 
     /**
-     * method to clean all the styling and setup again
+     *test method to clean all the styling and setup again
      *
      * @param actionEvent
      */
+
     public void reset(ActionEvent actionEvent) {
-        for (Button button : buttons) {
-            button.setStyle(null);
-        }
-        buttons.clear();
-        answers.clear();
-        question.setText(null); //this is not necessary, but, just in case, and its only one line
-        //   setOnStage();
-
+model.reset(actionEvent);
     }
-
-    /**
-     * method to shuffle.first shuffles the String list of answers.then puts those string in buttons. then, calls
-     * setRightAnswer to set the button with the right answer
-     */
-    public void setShuffled() {
-        Collections.shuffle(answers);
-
-        answer1.setText(answers.get(0));
-        answer2.setText(answers.get(1));
-        answer3.setText(answers.get(2));
-        answer4.setText(answers.get(3));
-
-        setRightAnswer();
-    }
-
-    /**
-     * method to get the button with the right answer.
-     */
-    public void setRightAnswer() {
-        buttons.add(answer1);
-        buttons.add(answer2);
-        buttons.add(answer3);
-        buttons.add(answer4);
-
-        for (Button b : buttons) {
-            if (b.getText().equals(q.getRightAnswer()))
-                right = b;
-        }
-    }
-
-    public void setOnStage() {
-        q = takeAquestion();
-
-        question.setText(q.getQuestion());
-        answers.add(q.getAnswer2());
-        answers.add(q.getAnswer3());
-        answers.add(q.getAnswer4());
-        answers.add(q.getRightAnswer());
-        categoryL.setText(q.getCategory());
-        setShuffled();
-    }
-
-    //remade method, to not take randomly from the list. I suppose it comes random from the server??
-    public Question takeAquestion() {
-
-        Question question = questionList.get(questionList.size() - 1);
-        questionList.remove(questionList.size() - 1);
-
-        return question;
-    }
-/*
-    public Question takeAquestion() {
-        refillListIfEmpty();
-        Random rnd = new Random();
-        int index = rnd.nextInt(questionList.size());
-        Question question = questionList.get(index);
-        questionList.remove(index);
-
-        return question;
-    }
-
-
-
-
-
-    public void refillListIfEmpty() {
-        if (questionList.size() < 1) {
-            questionList = new ArrayList<>(questionsAndAnswers.getQuestionList());
-        }
-    }
-
- */
-
 
 }
 
