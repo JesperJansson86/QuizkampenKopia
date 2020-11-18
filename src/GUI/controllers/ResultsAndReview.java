cb841342f34b20efccf4fde06475bed9dd6fcdd8
BuildTools <unconfigured@null.spigotmc.org>
2020-11-18 10:18:56 +0100

    PointCounter class that counts points for the client player. calculates round and game total points. (not yet displayed on Result screen)

--- src/GUI/controllers/ResultsAndReview.java
+++ src/GUI/controllers/ResultsAndReview.java
@@ -1,5 +1,6 @@
 package GUI.controllers;
 
+import MainClasses.PointCount;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
 
@@ -15,8 +16,45 @@ public class ResultsAndReview {
     public Label player2L;
     public Group roundGroup;
     public Label roundNumbL;
-    public Label player1Points;
+    public static Label player1Points;
     public Label player2Points;
+    public static Label player1PointsSum;
     public Label player2PointsSum;
-    public Label player1PointsSum;
+
+
+    /**
+     * the player1 result for the round
+     *
+     * @param p1Points the player1 result for the current round
+     */
+    public static void setPlayer1Points(int p1Points) {
+        player1Points.setText(String.valueOf(p1Points));
+    }
+
+    /**
+     * the player1 result for the round
+     *
+     * @param p2Points the player2 result for the current round
+     */
+    public void setPlayer2Points(int p2Points) {
+        player1Points.setText(String.valueOf(p2Points));
+    }
+
+    /**
+     * Player1s result for all the rounds
+     *
+     * @param player1Sum the total result of all the rounds (player1)
+     */
+    public static void setPlayer1PointsSum(int player1Sum) {
+        player1Points.setText(String.valueOf(player1Sum));
+    }
+
+    /**
+     * Player1s result for all the rounds
+     *
+     * @param player2Sum the total result of all the rounds (player2)
+     */
+    public void setPlayer2PointsSum(int player2Sum) {
+        player1Points.setText(String.valueOf(player2Sum));
+    }
 }
---
+++ src/MainClasses/PointCount.java
@@ -0,0 +1,107 @@
+package MainClasses;
+
+import java.util.Arrays;
+
+/**
+ * Created by Lukas Aronsson
+ * Date: 14/11/2020
+ * Time: 16:01
+ * Project: QuizkampenKopia
+ * Copyright: MIT
+ **/
+public class PointCount {
+
+    static int nbrRounds = 2; //the number of rounds that will be played //Changed in properties eventually
+
+    static int nbrQuestions = 2; //the number of questions in a round//Changed in properties eventually
+
+
+
+    static int[][] pointHolder = new int[nbrRounds][nbrQuestions]; //holds the points
+
+    static int[] pointRoundTotal = new int[nbrRounds]; //holds the totals of the round
+
+
+
+    public int getNbrRounds() {
+        return nbrRounds;
+    }
+
+    public void setNbrRounds(int nbr3Rounds) {
+        nbrRounds = nbr3Rounds;
+    }
+
+    public int getNbrQuestions() {
+        return nbrQuestions;
+    }
+
+    public void setNbrQuestions(int nbr3Questions) {
+        nbrQuestions = nbr3Questions;
+    }
+
+    public static int[][] getPointHolder() {
+        return pointHolder;
+    }
+
+    public static void setPointHolder(int[][] pointHolder) {
+        PointCount.pointHolder = pointHolder;
+    }
+
+    public static int[] getPointRoundTotal() {
+        return pointRoundTotal;
+    }
+
+    public static void setPointRoundTotal(int[] pointRoundTotal) {
+        PointCount.pointRoundTotal = pointRoundTotal;
+    }
+
+    /**
+     * adds 1 or 0 to pointHolder
+     *
+     * @param point  1 or 0 depending on correct answer or not
+     */
+    public static void playerPointCount(int point,int round,int questions){
+
+        pointHolder[round-1][questions-1] = point;
+
+        System.out.println(Arrays.toString(pointHolder[round-1]));//TEST: printout of the ArrayList
+
+    }
+
+    /**
+     * counts and returns the pointTotal of the round.
+     *
+     * @return returns the pointTotal of the round
+     */
+    public static int playerRoundTotal(int round){
+
+        //for loops that adds the ints in pointHolder to pointTotal
+        for (int integer : pointHolder[round-1]) {
+
+            pointRoundTotal[round-1] = pointRoundTotal[round-1] + integer; //adds the number in point holder to the point total
+        }
+
+        System.out.println("Round total:" + pointRoundTotal[round-1]);
+
+        return pointRoundTotal[round-1];
+    }
+
+    /**
+     *
+     * @return returns the total score for all the rounds in the game
+     */
+    public static  int playerGameTotal( ){
+
+
+        int total = 0;
+
+        for (int i = 0; i < nbrRounds; i++){
+
+            total = total + pointRoundTotal[i];
+
+        }
+
+        return total;
+    }
+
+}
