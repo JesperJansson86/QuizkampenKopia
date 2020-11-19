c9ef3635fb6d34e4908fb55640b712c88e0e5364
hodeiez <hodeinaiz@hotmail.com>
2020-11-19 09:59:31 +0100

    designing waitingPanel

--- src/GUI/controllers/Waiting.java
+++ src/GUI/controllers/Waiting.java
@@ -1,5 +1,8 @@
 package GUI.controllers;
 
+import javafx.scene.Group;
+import javafx.scene.shape.Circle;
+
 /**
  * Created by Hodei Eceiza
  * Date: 11/19/2020
@@ -8,4 +11,9 @@ package GUI.controllers;
  * Copyright: MIT
  */
 public class Waiting {
+    public Group circleGroup;
+    public Circle circleRed;
+    public Circle circleBlue;
+    public Circle circleYellow;
+    public Circle circleGreen;
 }
