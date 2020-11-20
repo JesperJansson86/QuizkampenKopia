bfb20da63498afa599ef0de653822cad23b807c8
kdhedman <kenth.david.hedman@gmail.com>
2020-11-20 15:33:56 +0100

    Fixes in Main, nonResizable and System.exit(0) on close.

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -24,8 +24,6 @@ public class Client implements Runnable {
         try (Socket socket = new Socket("localhost", Server.port)) {
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            Scanner scan = new Scanner(System.in);
-            String test = null;
 
             System.out.println("in Client, just before taking name");
             String name = (String) toClient.take();
@@ -63,15 +61,7 @@ public class Client implements Runnable {
 
                 out.writeObject(gr);
                 goToWaiting("End of Round, waiting for opponent/server");
-//                gr = runda(gr);
-//                if (gr.playerTurn % 2 == 1) System.out.println(gr.player1Results);
-//                else System.out.println(gr.player2Results);
-////                out.writeObject(gr);
-////                gr = gr;
-//                if (gr.gameover && gr.playerTurn % 2 == 0) break;
             }
-//            if (gr.playerTurn % 2 == 1) System.out.println(gr.player1Results);
-//            else System.out.println(gr.player2Results);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -140,53 +130,4 @@ public class Client implements Runnable {
             e.printStackTrace();
         }
     }
-
-    public static GameRound runda(GameRound gr) {
-        int count = 0;
-        while (!gr.qlist.get(count).getCategory().equals(gr.category)) {
-            count++;
-        }
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Kategori " + gr.category);
-        System.out.println("GameId " + gr.gameid);
-        for (int i = 1; i <= 1; i++) {
-            System.out.println("Svara på fråga: " + gr.qlist.get(i + count).getQuestion());
-            System.out.println(gr.qlist.get(i + count).getRightAnswer());
-            System.out.println(gr.qlist.get(i + count).getAnswer2());
-            System.out.println(gr.qlist.get(i + count).getAnswer3());
-            System.out.println(gr.qlist.get(i + count).getAnswer4());
-            int choice = Integer.parseInt(scan.next());
-            String answer;
-            switch (choice) {
-                case 1: {
-                    answer = gr.qlist.get(i + count).getRightAnswer();
-                    break;
-                }
-                case 2: {
-                    answer = gr.qlist.get(i + count).getAnswer2();
-                    break;
-                }
-                case 3: {
-                    answer = gr.qlist.get(i + count).getAnswer3();
-                    break;
-                }
-                case 4: {
-                    answer = gr.qlist.get(i + count).getRightAnswer();
-                    break;
-                }
-
-                default:
-                    throw new IllegalStateException("Unexpected value: " + choice);
-            }
-
-            if (gr.playerTurn % 2 == 1) gr.player1Results.add(answer);
-            else gr.player2Results.add(answer);
-        }
-        return gr;
-    }
-//    public void grading(MainClasses.GameRound gr){
-//        for (int i = 0; i < gr.player1Results.size(); i++) {
-//        if (gr.player1Results.get(i).equals())
-//        }
-//    }
 }
