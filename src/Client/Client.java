6691df55c27778058b09f9fcb70554dadc217c55
kdhedman <kenth.david.hedman@gmail.com>
2020-11-20 09:15:27 +0100

    Added Jsoup for parsing HTML text in QuestionsFactory. UglyPatch in QuestionFactory to only load initial questions once, constructor was called twice and questions duplicated.

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -10,13 +10,14 @@ import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class Client implements Runnable{
+public class Client implements Runnable {
     static GameRound gr;
     boolean player1;
     public static BlockingQueue<Object> toGUI = new LinkedBlockingQueue();
     public static BlockingQueue<Object> toClient = new LinkedBlockingQueue();
 
-    public Client() {}
+    public Client() {
+    }
 
     @Override
     public void run() {
@@ -31,23 +32,32 @@ public class Client implements Runnable{
             while (true) {
 
                 System.out.println("in Client, just before add name");
-                gr = (GameRound) in.readObject(); //add names i needed
-                if(gr.playerNames.size() < 1 || gr.roundnumber == 1){
+                gr = (GameRound) in.readObject(); //add names if needed, ergo, first round.
+                if (gr.playerNames.size() < 1 || gr.roundnumber == 1) {
                     player1 = true;
                     gr.playerNames.add(name);
-                } else if (gr.roundnumber == 1){
+                } else if (gr.roundnumber == 1) {
                     gr.playerNames.add(name);
                 }
 
-                System.out.println("in Client gr.category = " + gr.category);;
+                System.out.println("in Client gr.category = " + gr.category);
+
                 if (gr.category == null) { //Choose category and answer questions
                     System.out.println("in Client: gr.category is null.");
                     getCategory();
                     System.out.println("in Client: gr.category is now: " + gr.category);
                     answerQuestions();
-
-//
+                    out.writeObject(gr);
+                    showResults();
+                    //send answers to Server and await response.
 //                    if (gr.gameover && gr.playerTurn % 2 == 1) break;
+                } else if (gr.category != null) {
+                    System.out.println("in Client: gr.category is " + gr.category);
+                    answerQuestions();
+                    getCategory();
+                    answerQuestions();
+                    out.writeObject(gr);
+                    showResults();
                 }
 
 
@@ -70,7 +80,7 @@ public class Client implements Runnable{
         }
     }
 
-    private void getCategory(){
+    private void getCategory() {
         try {
             System.out.println("GetCategory() toGUI.put CATEGORY");
             toGUI.put("CATEGORY");
@@ -88,23 +98,37 @@ public class Client implements Runnable{
 
     }
 
-    public void answerQuestions(){
+    public void answerQuestions() {
         try {
             List<Question> activeQuestions = new ArrayList<>();
-            for(Question q: gr.qlist){
-                if(q.getCategory().equalsIgnoreCase(gr.category)){
+            for (Question q : gr.qlist) {
+                if (q.getCategory().equalsIgnoreCase(gr.category)) {
                     activeQuestions.add(q);
                 }
             }
-                toGUI.put("QUESTION");
-                toGUI.put(activeQuestions);
-                toGUI.put(gr.roundnumber);
-                ArrayList<String> result = (ArrayList<String>) toClient.take();
-
+            toGUI.put("QUESTION");
+            toGUI.put(activeQuestions);
+            toGUI.put(gr.roundnumber);
+            ArrayList<String> result = (ArrayList<String>) toClient.take();
+            for (String answer : result) {
+                if (player1) {
+                    gr.player1Results.add(answer);
+                } else {
+                    gr.player2Results.add(answer);
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    public void showResults(){
+        try{
+            toGUI.put("RESULTS");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static GameRound runda(GameRound gr) {
