d1a91a21188a47ce875c21e96ea18f5c53ab2c6b
BuildTools <unconfigured@null.spigotmc.org>
2020-11-21 16:08:56 +0100

    Deleted .idea (aka all xml files) hopefully it fixes something.

--- src/GUI/models/QuestionsPanelModel.java
+++ src/GUI/models/QuestionsPanelModel.java
@@ -66,7 +66,7 @@ public class QuestionsPanelModel {
         buttons.clear();
         answers.clear();
         question.setText(null); //this is not necessary, but, just in case, and its only one line
-         // setOnStage();
+        // setOnStage();
     }
 
     /**
