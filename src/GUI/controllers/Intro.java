9dd505079b57070870a41c0bf245324846e2575f
Pavel Barros Quintanilla <Pavelbarrosq@gmail.com>
2020-11-25 16:55:38 +0100

    Changed intro gui added more buttons with inputfields, created a sql query with 3 users, connected sql with code using localhost. (Progress)

--- src/GUI/controllers/Intro.java
+++ src/GUI/controllers/Intro.java
@@ -1,6 +1,7 @@
 package GUI.controllers;
 
 import Client.Client;
+import GUI.models.DatabaseConnection;
 import GUI.models.GUIutils;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
@@ -11,6 +12,10 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -21,32 +26,80 @@ import java.util.concurrent.BlockingQueue;
  * Copyright: MIT
  */
 public class Intro {
+    public TextField passwordField;
+    public Button startB1;
+    public TextField usernameField;
+    public Button loginButton;
+    public Button continueButton;
+    public Button registerButton;
     BlockingQueue toGUI = Client.toGUI;
     BlockingQueue toClient = Client.toClient;
-    public Label message;
-    public Button startB;
+    //public Label message;
+   // public Button startB;
     public TextField nameField;
     public AnchorPane introPane;
+    public UUID uuid;
     GUIutils util;
 
     public void initialize() {
-        message.setText(null);
+        //message.setText(null);
         util = new GUIutils(introPane);
     }
 
-    public void startButtonOn() {
-        startB.setDisable(true);
-        if (!nameField.getText().isBlank() || !nameField.getText().isEmpty()) {
+
+    public void loginButtonOn(ActionEvent actionEvent) {
+
+        if (!usernameField.getText().isEmpty() && !passwordField.getText().isEmpty()) {
+            DatabaseConnection connectNow = new DatabaseConnection();
+            System.out.println(connectNow.databaseLink);
+            System.out.println(connectNow.getConnection().toString());
+            Connection connectDb = connectNow.getConnection();
+            System.out.println(connectDb);
+
+            String verifyLogin = "SELECT count(1) FROM user_account WHERE username = '" + usernameField.getText() + "' AND password ='" + passwordField.getText() + "'";
+
+            System.out.println(usernameField.getText());
+            System.out.println(passwordField.getText());
             try {
-                toClient.put(nameField.getText());
-                String nextScene = (String) toGUI.take();
-                util.changeSceneNew(nextScene);
-            } catch (InterruptedException | IOException e) {
+                System.out.println("HERE1");
+                Statement statement = connectDb.createStatement();
+
+                ResultSet queryResult = statement.executeQuery(verifyLogin);
+                System.out.println("Inside try");
+                System.out.println(queryResult);
+
+                while (queryResult.next()) {
+                    if (queryResult.getInt(1) == 1) {
+                        System.out.println("Logged in successfully");
+                    } else {
+                        System.out.println("Invalid login");
+                    }
+
+                }
+            } catch (Exception e) {
                 e.printStackTrace();
+                System.out.println("nooooot");
             }
-        } else {
-            message.setText("Write something in the name field, please.");
-            startB.setDisable(false);
+        }
+
+
+    }
+
+
+    public void continueButtonOn(ActionEvent actionEvent) {
+        uuid = UUID.randomUUID();
+        try {
+            toClient.put(uuid.toString());
+            String nextScene = (String) toGUI.take();
+            util.changeSceneNew(nextScene);
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            System.out.println("Något gick fel");
         }
     }
+
+    public void registerButtonOn(ActionEvent actionEvent) {
+    }
+
+
 }
---
+++ src/GUI/models/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package GUI.models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public Connection databaseLink;
+
+    public Connection getConnection() {
+        String databaseName = "quizkampen_db_new";
+        String databaseUser = "root";
+        String databasePassword = "password1234";
+        String url = "jdbc:mysql://localhost/" + databaseName;
+
+        try {
+            System.out.println("in try databaseconnection");
+            Class.forName("com.mysql.jdbc.Driver");
+            databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+            System.out.println("Success connection");
+            System.out.println(databaseLink);
+            return databaseLink;
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+
+        }
+
+        return null;
+    }
+}
