daf89c42218f317d684e3838677222cc61fb9fc1
Jesper Jansson <jesper.jansson@yh.nackademin.se>
2020-11-26 13:47:29 +0100

    Städat i GameRound

--- src/MainClasses/GameRound.java
+++ src/MainClasses/GameRound.java
@@ -1,8 +1,5 @@
 package MainClasses;
 
-import MainClasses.Question;
-import MainClasses.QuestionFactory;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,13 +8,9 @@ public class GameRound implements Serializable {
     public int gameid = 1;
     public int roundnumber = 1;
     public int playerTurn = 0;
-    public int amountOfRounds;
-    public int questionsPerRound;
     public String category = null;
     public boolean gameover = false;
-    public List<String> playedCategorys = new ArrayList<>();
     public List<String> categoryList = new ArrayList<>();
-    public List<Question> oldQuestionslist = new ArrayList<>();
     public List<Question> qlist = new ArrayList<>();
     public List<String> player1Results = new ArrayList<>();
     public List<String> player2Results = new ArrayList<>();
@@ -25,14 +18,6 @@ public class GameRound implements Serializable {
     public List<Integer> player1Score = new ArrayList<>();
     public List<Integer> player2Score = new ArrayList<>();
     public List<Question> activeQuestions = new ArrayList();
-
-
-//    public MainClasses.GameRound(int gameid, List qlist) {
-//        this.gameid = gameid;
-//        this.amountOfRounds= amountOfRounds;
-//        this.questionsPerRound=questionsPerRound;
-//        this.qlist=qlist;
-//    }
 
 
 }
